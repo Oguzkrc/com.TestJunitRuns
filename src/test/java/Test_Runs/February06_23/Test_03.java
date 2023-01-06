11b8c607161608b2c3206f808bd591457e5a4853
Oguz Karaca <oguzkaraca92@gmail.com>
2023-01-06 21:25:59 +0300

    first commit

---
+++ src/test/java/Test_Runs/February06_23/Test_03.java
@@ -0,0 +1,11 @@
+package Test_Runs.February06_23;
+
+public class Test_03 {
+
+    //1- https://www.qualitydemy.com/ anasayfasina gidin
+    //2- login linkine basin
+    // 3- Kullanici email’i olarak valid email girin
+    // 4- Kullanici sifresi olarak valid sifre girin
+    // 5- Login butonuna basarak login olun
+    // 6- Basarili olarak giris yapilabildigini test edin
+}
